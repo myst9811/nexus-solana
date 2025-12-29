56761593f3d0f2683bfb353472ea728e6274e802
Shannen Saikia <shannen.saikia@gmail.com>
2025-12-29 19:28:36 +0530

    fix: anchor syntax constraints

--- programs/nexus-bridge/src/instructions/unlock_tokens.rs
+++ programs/nexus-bridge/src/instructions/unlock_tokens.rs
@@ -18,7 +18,7 @@ pub struct UnlockTokens<'info> {
         init,
         payer = authority,
         space = ProcessedTransaction::LEN,
-        seeds = [b"processed_tx", eth_tx_hash.as_bytes()],
+        seeds = [b"processed_tx", eth_tx_hash.as_ref()],
         bump
     )]
     pub processed_tx: Account<'info, ProcessedTransaction>,
